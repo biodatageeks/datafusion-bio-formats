4b8e09140da60ddd71569fae404474bb7cf2af39
Marek Wieiwórka <marek.wiewiorka@gmail.com>
2026-05-18 04:53:47 +0200

    feat: add bigwig and bigbed providers

---
+++ datafusion/bio-format-bbi/src/lib.rs
@@ -0,0 +1,8 @@
+//! BigWig and BigBed file format support for Apache DataFusion.
+
+#![warn(missing_docs)]
+
+/// BigBed table provider.
+pub mod bigbed;
+/// BigWig table provider.
+pub mod bigwig;
